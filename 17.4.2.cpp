f50c240c780f1e27306b45b2392eb09a3e03c2c6
Alexandr <sasha_stepanov94@mail.ru>
2024-06-20 18:21:03 +0300

    Complite v1.3

--- 17.4.2.cpp
+++ 17.4.2.cpp
@@ -19,13 +19,12 @@ int main() {
   std::cout << "Reverse you order of int variables\n";
 
   int arr[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-  int sizeArr = std::size(arr); // как размер массива в функции использовать напрямую, я так и не понял
-
+  
   std::cout << "Your order of int variables: \t\t";
-  printArr(arr, sizeArr);
+  printArr(arr, std::size(arr));
 
-  in_reverse_order(arr, sizeArr);
+  in_reverse_order(arr, std::size(arr));
 
   std::cout << "You reverse order of int variables: \t";
-  printArr(arr, sizeArr);
+  printArr(arr, std::size(arr));
 }
